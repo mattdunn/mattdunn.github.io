9966d56bf68d8fda9bfb9e6a238ee151e7ba1114
mattdunn <contact@mattonsoftware.com>
2012-09-25 15:29:37 +0100

    update icons, fade in slowly (icons after lettering)

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,8 @@
 (function($) {
   $(function() {
-    $(".spiffy-text").fadeIn('slow').lettering();
+    $(".spiffy-text").lettering(); 
+    $(".spiffy-text").fadeIn(1000, function(){
+      $("#where-to-find-me a").fadeIn(1000);
+    });
   });
 })(jQuery);
